887de21f61ba4f89b987d52116fe294a7c82207a
junsu0302 <junsu96378224@gmail.com>
2024-05-09 01:52:31 +0900

    #1 - fontsize 정의
    프로젝트가 진행되면서 모든 페이지의 폰트 크기를 유지하기 위해 폰트를 미리 정의
    
    차후 스켈레톤 구현을 위해 영역 높이도 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,72 @@ const config: Config = {
       md: { min: "576px", max: "1024px" }, // 모바일 가로, 태블릿 세로
       lg: { min: "1025px" }, // 태블릿 가로, 데스크탑
     },
+    fontSize: {
+      sm: [
+        "14px",
+        {
+          lineHeight: "20px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      md: [
+        "16px",
+        {
+          lineHeight: "24px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      lg: [
+        "18px",
+        {
+          lineHeight: "28px",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      h1: [
+        "48px",
+        {
+          lineHeight: "72px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+      h2: [
+        "40px",
+        {
+          lineHeight: "60px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+      h3: [
+        "32px",
+        {
+          lineHeight: "48px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+      h4: [
+        "28px",
+        {
+          lineHeight: "42px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+      h5: [
+        "24px",
+        {
+          lineHeight: "36px",
+          letterSpacing: "0",
+          fontWeight: "700",
+        },
+      ],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",

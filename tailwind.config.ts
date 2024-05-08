97973142eccd6b1650c943df240c04ae85fab65b
junsu0302 <junsu96378224@gmail.com>
2024-05-09 01:51:04 +0900

    #1 - screens(화면 크기) 정의
    기기에 따른(데스크탑, 테블릿, 모바일) 맞춤 UI 제공을 위한 화면 크기 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: { max: "575px" }, // 모바일 세로
+      md: { min: "576px", max: "1024px" }, // 모바일 가로, 태블릿 세로
+      lg: { min: "1025px" }, // 태블릿 가로, 데스크탑
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",

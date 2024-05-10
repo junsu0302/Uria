858e74060fc0e5f493ec1f67fd5e31e94c03eaf1
junsu0302 <junsu96378224@gmail.com>
2024-05-10 19:34:57 +0900

    #2 - Flex (shared)
    요소들이 영역의 양 끝단을 기준으로 가운데 정렬하는 컴포넌트

---
+++ src/shared/layouts/Flex.tsx
@@ -0,0 +1,24 @@
+import "@/styles/layouts/Flex.scss";
+import Box from "./Box";
+
+interface CenterProps {
+  children?: React.ReactNode;
+  type?: "deep" | "soft" | "light" | "background" | string;
+  color?: string;
+  size?: string;
+  className?: string;
+}
+
+export default function Center({
+  children,
+  type,
+  color,
+  size,
+  className,
+}: CenterProps) {
+  return (
+    <Box className={`flex-between ${type} ${color} ${size} ${className}`}>
+      {children}
+    </Box>
+  );
+}
